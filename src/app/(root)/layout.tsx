9b7a96efd19140dabe5d23bd3eb9c0d341eb2e08
Alisson <alissoneatena1@gmail.com>
2025-02-22 09:21:06 -0300

    feat: create header and footer

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,3 +1,5 @@
+import { Footer } from "@/components/footer";
+import { Header } from "@/components/shared/header";
 
  const RootLayout = ({
     children,
@@ -6,9 +8,12 @@
 }>) => {
     return (
         <div className="flex h-screen flex-col">
+            <Header />
             <main className="flex-1 wrapper">
                 {children}
             </main>
+
+            <Footer />
         </div>
     )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/assets/globals.css";
-import { APP_NAME } from "@/lib/constants";
+import { APP_DESCRIPTION, APP_NAME, SERVER_URL } from "@/lib/constants";
 
 
 const inter = Inter({
@@ -9,8 +9,12 @@ const inter = Inter({
 })
 
 export const metadata: Metadata = {
-  title: APP_NAME,
-  description: "A modern ecommerce platform built with Next.js",
+  title: {
+    template: `%s | ${APP_NAME}`,
+    default: APP_NAME,
+  },
+  description: APP_DESCRIPTION,
+  metadataBase: new URL(SERVER_URL)
 };
 
 export default function RootLayout({
